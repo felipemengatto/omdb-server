9e8f4862d33633275bad62ee2f843e2ca6cd9a9d
Felipe Mengatto <felipemengatto@gmail.com>
2019-02-21 01:08:09 -0300

    concertado cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const express = require('express');
-const cors = require('cors');
 const omdb = new (require('omdbapi'))('fa3bee26');
 const app = express();
 
-app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/search/:search', async (req, res) => {
     const search = req.param('search');
@@ -36,4 +39,4 @@ app.get('/movie/:id', async (req, res) => {
 
 });
 
-http.createServer(app).listen(80, () => console.log('Server is Up!'));
+http.createServer(app).listen(3000, () => console.log('Server is Up!'));
